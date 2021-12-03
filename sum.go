5368c176363ab87ff7e4b4d8481262c40b6fdec9
ybb <786373983@qq.com>
2021-12-03 10:42:06 +0800

    SumAll

--- sum.go
+++ sum.go
@@ -12,3 +12,14 @@ func Sum(arr []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return
+}
